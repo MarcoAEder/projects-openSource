5ba801918923eb9231bb484fb37629e194da4bca
Marco Ariel Eder <86081972+MarcoAEder@users.noreply.github.com>
2024-06-10 09:44:40 -0300

    Delete "automacoes/buscador_precos" directory.

--- automacoes/buscador_precos/main.py
+++
@@ -1,6 +0,0 @@
-from models.message import * # NOTE: Modelo de importação. Remover quando arquivos estiverem organizados.
-
-
-# NOTE: Arquivo principal de execução.
-def main():
-   return print(helloWorld())
--- automacoes/buscador_precos/models/message.py
+++
@@ -1,6 +0,0 @@
-import os  # NOTE: Modelo de importação. Remover quando arquivos estiverem organizados.
-
-
-# NOTE: Arquivo de classes, módulos e/ou funções.
-def helloWorld():
-    return "Hello, world!"
